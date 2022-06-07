2bddd28d07d023a16325696704e095c76f6d361d
esadakman <esadd26@gmail.com>
2022-06-08 00:10:56 +0300

    button events add

--- assets/app.js
+++ assets/app.js
@@ -1,28 +1,49 @@
 console.log("%ccalculator is running...", "color: red");
-
-const displayScreen = document.querySelector(".display");
-
-window.addEventListener("load", () => {});
-
+// window.addEventListener("load", () => {});
+let displayScreen = document.querySelector(".display");
+let topDisplay = displayScreen.firstElementChild;
+let bottomDisplay = displayScreen.lastElementChild;
 let buttonsDiv = document.querySelector(".buttons");
 // let classes = buttonsDiv.classList;
 
 buttonsDiv.addEventListener("click", (e) => {
+  // let bottt = bottomDisplay.innerHTML;
+  // bottomDisplay.innerHTML = bottomDisplay.innerHTML.substr(0, 10);
+  // topDisplay.innerHTML = topDisplay.innerHTML.substr(0, 10);
   if (e.target.classList.contains("AC")) {
-    // console.log(e.target);
-    displayScreen.firstElementChild.innerHTML = "asddd";
-    console.log("AC CLICK");
+    displayReset(bottomDisplay);
+    displayReset(topDisplay);
     // e.preventDefault();
+  } else if (e.target.classList.contains("±")) {
+    if (bottomDisplay.innerHTML[0] === "-") {
+      bottomDisplay.innerHTML = bottomDisplay.innerHTML.substring(1);
+    } else {
+      bottomDisplay.innerHTML = "-" + bottomDisplay.innerHTML;
+    }
+  } else if (e.target.classList.contains("%")) {
+    bottomDisplay.innerHTML = bottomDisplay.innerHTML / 100;
+  } else if (e.target.classList.contains("ops")) {
+    operation(e.target.innerHTML);
+  } else if (e.target.classList.contains("nums")) {
+    addNumber(e.target.innerHTML);
+  } else if (e.target.classList.contains(".")) {
+    addNumber(".");
+  } else if (e.target.classList.contains("=")) {
+    bottomDisplay.innerHTML =
+      ~~bottomDisplay.innerHTML + ~~topDisplay.innerHTML;
+    displayReset(topDisplay);
   }
 });
 
-// let ac = document.querySelector(".AC");
+function addNumber(content) {
+  bottomDisplay.innerHTML += content;
+}
 
-// ac.addEventListener("click", function (e) {
-//   // console.log(hak);
-//   console.log("asds");
-//   document.querySelector(".display").firstElementChild.innerHTML = "asddd";
-//   e.preventDefault();
-// });
+function displayReset(variable) {
+  variable.innerHTML = "";
+}
 
-// document.querySelector(".buttons").classList.contains("AC");
+function operation(x) {
+  topDisplay.innerHTML = bottomDisplay.innerHTML + x;
+  displayReset(bottomDisplay);
+}
